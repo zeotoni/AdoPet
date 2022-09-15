320655cacf7a6fa474af50761eca44c3d35e728c
zeotoni <zeotoni666@gmail.com>
2022-09-15 18:17:18 -0300

    Logica de mudar senha

--- js/mudarSenha.js
+++ js/mudarSenha.js
@@ -1,11 +1,12 @@
 import { validaInput } from './validacao.js';
+
 const pegaUrl = new URL(window.location);
 const id = pegaUrl.searchParams.get('id');
 
 const inputSenha = document.querySelector('[data-senha]');
 const inputSenhaConfirmar = document.querySelector('[data-senha-confirmar]');
 const btnMudarSenha = document.querySelector('[data-mudar-senha]');
-const msgSucess = document.querySelector('[data-msg-sucess]')
+const msgSucess = document.querySelector('[data-msg-sucess]');
 
 inputSenha.addEventListener('blur', () => {
     validaInput(inputSenha, /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*[ !@#$%^&*_=+-]).{6,12}$/, "A senha deve conter entre 6 a 12 caracteres, deve conter números, letras maiúsculas e não deve conter caracteres especiais!")
@@ -15,7 +16,7 @@ inputSenhaConfirmar.addEventListener('blur', ()=> {
     validaInput(inputSenhaConfirmar, inputSenha.value, "As senhas não conferem")
 });
 
-btnMudarSenha.addEventListener('click', (e) => {
+btnMudarSenha.addEventListener('submit', (e) => {
     e.preventDefault();
     let novaSenha = { "senhaSv": inputSenhaConfirmar.value}
     fetch(`https://arquivo-json-adopet.herokuapp.com/usuarios/${id}`, {
